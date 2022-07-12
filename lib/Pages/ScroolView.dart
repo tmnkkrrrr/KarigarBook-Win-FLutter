7dd98d279681a9056b79227a9603365e7b44ef65
tmnkakr <tmnkakr@gmail.com>
2022-07-12 14:23:24 +0530

    KarigarLeger Change

--- lib/Pages/ScroolView.dart
+++ lib/Pages/ScroolView.dart
@@ -1,85 +1,85 @@
-import 'package:flutter/material.dart';
-import 'package:pluto_grid/pluto_grid.dart';
-import 'package:syncfusion_flutter_datagrid/datagrid.dart';
+// import 'package:flutter/material.dart';
+// // import 'package:pluto_grid/pluto_grid.dart';
+// import 'package:syncfusion_flutter_datagrid/datagrid.dart';
 
-class Employee {
-  Employee(this.id, this.name, this.designation, this.salary);
-  final int id;
-  final String name;
-  final String designation;
-  final int salary;
-}
-
-// List<Employee> employees = <Employee>[];
-// late EmployeeDataSource employeeDataSource;
-
-// @override
-// void initState() {
-//  // super.initState();
-//   employees = getEmployees();
-//   employeeDataSource = EmployeeDataSource(employees: employees);
+// class Employee {
+//   Employee(this.id, this.name, this.designation, this.salary);
+//   final int id;
+//   final String name;
+//   final String designation;
+//   final int salary;
 // }
 
-List<Employee> getEmployees() {
-  return [
-    Employee(10001, 'James', 'Project Lead', 20000),
-    Employee(10002, 'Kathryn', 'Manager', 30000),
-    Employee(10003, 'Lara', 'Developer', 15000),
-    Employee(10004, 'Michael', 'Designer', 15000),
-    Employee(10005, 'Martin', 'Developer', 15000),
-    Employee(10006, 'Newberry', 'Developer', 15000),
-    Employee(10007, 'Balnc', 'Developer', 15000),
-    Employee(10008, 'Perry', 'Developer', 15000),
-    Employee(10009, 'Gable', 'Developer', 15000),
-    Employee(10010, 'Grimes', 'Developer', 15000)
-  ];
-}
+// // List<Employee> employees = <Employee>[];
+// // late EmployeeDataSource employeeDataSource;
 
-class EmployeeDataSource extends DataGridSource {
-  EmployeeDataSource(List<Employee> employees) {
-    dataGridRows = employees
-        .map<DataGridRow>((dataGridRow) => DataGridRow(cells: [
-              DataGridCell<int>(columnName: 'id', value: e.id),
-              DataGridCell<String>(columnName: 'name', value: e.name),
-              DataGridCell<String>(
-                  columnName: 'designation', value: e.designation),
-              DataGridCell<int>(columnName: 'salary', value: e.salary),
-            ]))
-        .toList();
-  }
+// // @override
+// // void initState() {
+// //  // super.initState();
+// //   employees = getEmployees();
+// //   employeeDataSource = EmployeeDataSource(employees: employees);
+// // }
 
-  late List<DataGridRow> dataGridRows;
+// List<Employee> getEmployees() {
+//   return [
+//     Employee(10001, 'James', 'Project Lead', 20000),
+//     Employee(10002, 'Kathryn', 'Manager', 30000),
+//     Employee(10003, 'Lara', 'Developer', 15000),
+//     Employee(10004, 'Michael', 'Designer', 15000),
+//     Employee(10005, 'Martin', 'Developer', 15000),
+//     Employee(10006, 'Newberry', 'Developer', 15000),
+//     Employee(10007, 'Balnc', 'Developer', 15000),
+//     Employee(10008, 'Perry', 'Developer', 15000),
+//     Employee(10009, 'Gable', 'Developer', 15000),
+//     Employee(10010, 'Grimes', 'Developer', 15000)
+//   ];
+// }
 
-  @override
-  List<DataGridRow> get rows => dataGridRows;
+// class EmployeeDataSource extends DataGridSource {
+//   EmployeeDataSource(List<Employee> employees) {
+//     dataGridRows = employees
+//         .map<DataGridRow>((dataGridRow) => DataGridRow(cells: [
+//               DataGridCell<int>(columnName: 'id', value: e.id),
+//               DataGridCell<String>(columnName: 'name', value: e.name),
+//               DataGridCell<String>(
+//                   columnName: 'designation', value: e.designation),
+//               DataGridCell<int>(columnName: 'salary', value: e.salary),
+//             ]))
+//         .toList();
+//   }
 
-  @override
-  DataGridRowAdapter? buildRow(DataGridRow row) {
-    return DataGridRowAdapter(
-        cells: row.getCells().map<Widget>((dataGridCell) {
-      return Container(
-        alignment: (dataGridCell.columnName == 'id' ||
-                dataGridCell.columnName == 'salary')
-            ? Alignment.centerRight
-            : Alignment.centerLeft,
-        padding: EdgeInsets.all(16.0),
-        child: Text(dataGridCell.value.toString()),
-      );
-    }).toList());
-  }
-}
+//   late List<DataGridRow> dataGridRows;
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-      appBar: AppBar(
-        title: Text('Syncfusion DataGrid'),
-      ),
-      body: Center(
-        child: Expanded(
-          child: SfDataGrid(
-            source: _employeeDataSource,
-          ),
-        ),
-      ));
-}
+//   @override
+//   List<DataGridRow> get rows => dataGridRows;
+
+//   @override
+//   DataGridRowAdapter? buildRow(DataGridRow row) {
+//     return DataGridRowAdapter(
+//         cells: row.getCells().map<Widget>((dataGridCell) {
+//       return Container(
+//         alignment: (dataGridCell.columnName == 'id' ||
+//                 dataGridCell.columnName == 'salary')
+//             ? Alignment.centerRight
+//             : Alignment.centerLeft,
+//         padding: EdgeInsets.all(16.0),
+//         child: Text(dataGridCell.value.toString()),
+//       );
+//     }).toList());
+//   }
+// }
+
+// @override
+// Widget build(BuildContext context) {
+//   return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Syncfusion DataGrid'),
+//       ),
+//       body: Center(
+//         child: Expanded(
+//           child: SfDataGrid(
+//             source: _employeeDataSource,
+//           ),
+//         ),
+//       ));
+// }
--- lib/Pages/karigarLedger.dart
+++ lib/Pages/karigarLedger.dart
@@ -1,5 +1,7 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
-import 'package:pluto_grid/pluto_grid.dart';
+// import 'package:pluto_grid/pluto_grid.dart';
 
 void main() {
   runApp(KarigarLedger());
@@ -9,150 +11,129 @@ class KarigarLedger extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // title: "Cocktail App",
-      theme: ThemeData(
-        primarySwatch: Colors.brown,
-        fontFamily: "Pacifico",
-      ),
-      home: Led(),
-    );
-  }
-}
-
-class Led extends StatelessWidget {
-  @override
-  Widget build(BuildContext) {
-    return MaterialApp(
         debugShowCheckedModeBanner: false,
+        // title: "Cocktail App",
+        theme: ThemeData(
+          primarySwatch: Colors.brown,
+          fontFamily: "Pacifico",
+        ),
         home: Scaffold(
-          appBar: AppBar(
-            title: Text('Karigar Ledger'),
-          ),
-          body: Column(children: [
-            Row(
-              children: <Widget>[
-                Expanded(
-                    child: OutlinedButton(
-                  child: Text(
-                    'Miraz KadaiWala',
-                    style: TextStyle(
-                      fontSize: 45,
-                    ),
-                  ),
-                  style: OutlinedButton.styleFrom(
-                    primary: Colors.white,
-                    backgroundColor: Colors.teal,
-                    shadowColor: Colors.red,
-                    elevation: 10,
-                  ),
-                  onPressed: () {
-                    print('Pressed');
-                  },
-                )),
-                Expanded(
-                    child: OutlinedButton(
-                  child: Text(
-                    'Total Pending Pc : 46 ',
-                    style: TextStyle(
-                      fontSize: 45,
-                    ),
-                  ),
-                  style: OutlinedButton.styleFrom(
-                    primary: Colors.white,
-                    backgroundColor: Color.fromARGB(255, 178, 173, 35),
-                    shadowColor: Color.fromARGB(255, 54, 244, 127),
-                    elevation: 10,
-                  ),
-                  onPressed: () {
-                    print('Pressed');
-                  },
-                )),
-              ],
-            ),
-            Row(children: <Widget>[
-              Expanded(
-                  child: OutlinedButton(
-                child: Text(
-                  'Address : ...............................',
-                  style: TextStyle(
-                    fontSize: 25,
-                  ),
+            body: Column(children: <Widget>[
+          // Expanded(
+          // child:
+          Row(children: <Widget>[
+            Expanded(
+                child: OutlinedButton(
+              child: const Text(
+                'Miraz KadaiWala',
+                style: const TextStyle(
+                  fontSize: 45,
                 ),
-                style: OutlinedButton.styleFrom(
-                  primary: Colors.white,
-                  backgroundColor: Color.fromARGB(255, 200, 200, 149),
-                  shadowColor: Colors.red,
-                  elevation: 10,
+              ),
+              style: OutlinedButton.styleFrom(
+                primary: Colors.white,
+                backgroundColor: Colors.teal,
+                shadowColor: Colors.red,
+                elevation: 10,
+              ),
+              onPressed: () {
+                print('Pressed');
+              },
+            )),
+            Expanded(
+                child: OutlinedButton(
+              child: const Text(
+                'Total Pending Pc : 46 ',
+                style: const TextStyle(
+                  fontSize: 45,
                 ),
-                onPressed: () {
-                  print('Pressed');
-                },
-              )),
-            ]),
-            Row(
-              children: <Widget>[
-                Container(
-                  padding: const EdgeInsets.all(1),
-                  child: PlutoGrid(
-                      columns: columns,
-                      rows: rows,
-                      onChanged: (PlutoGridOnChangedEvent event) {
-                        print(event);
-                      },
-                      onLoaded: (PlutoGridOnLoadedEvent event) {
-                        print(event);
-                      }),
+              ),
+              style: OutlinedButton.styleFrom(
+                primary: Colors.white,
+                backgroundColor: const Color.fromARGB(255, 178, 173, 35),
+                shadowColor: const Color.fromARGB(255, 54, 244, 127),
+                elevation: 10,
+              ),
+              onPressed: () {
+                print('Pressed');
+              },
+            )
+                // ),
+
                 ),
+
+            //
+          ]),
+          Expanded(
+              child: Row(children: <Widget>[
+            GridView(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+              ),
+              children: [
+                Image.network('https://picsum.photos/250?image=1'),
+                Image.network('https://picsum.photos/250?image=2'),
+                Image.network('https://picsum.photos/250?image=3'),
+                Image.network('https://picsum.photos/250?image=4'),
               ],
             )
-          ]),
-        ));
+          ])),
+        ])));
   }
 }
 
-List<PlutoColumn> columns = [
-  /// Text Column definition
-  PlutoColumn(
-    title: 'text column',
-    field: 'text_field',
-    type: PlutoColumnType.text(),
-  ),
+// class Led extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext) {
+//     return MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         home:
+//   }
+// }
 
-  /// Number Column definition
-  PlutoColumn(
-    title: 'number column',
-    field: 'number_field',
-    type: PlutoColumnType.number(),
-  ),
+// List<PlutoColumn> columns = [
+//   /// Text Column definition
+//   PlutoColumn(
+//     title: 'text column',
+//     field: 'text_field',
+//     type: PlutoColumnType.text(),
+//   ),
 
-  /// Select Column definition
-  PlutoColumn(
-    title: 'select column',
-    field: 'select_field',
-    type: PlutoColumnType.select(['item1', 'item2', 'item3']),
-  ),
+//   /// Number Column definition
+//   PlutoColumn(
+//     title: 'number column',
+//     field: 'number_field',
+//     type: PlutoColumnType.number(),
+//   ),
 
-  /// Datetime Column definition
-];
+//   /// Select Column definition
+//   PlutoColumn(
+//     title: 'select column',
+//     field: 'select_field',
+//     type: PlutoColumnType.select(['item1', 'item2', 'item3']),
+//   ),
 
-List<PlutoRow> rows = [
-  PlutoRow(cells: {
-    'text_field': PlutoCell(value: 'Text cell value1'),
-    'number_field': PlutoCell(value: 2020),
-    'select_field': PlutoCell(value: 'item1'),
-  }),
-  PlutoRow(
-    cells: {
-      'text_field': PlutoCell(value: 'Text cell value2'),
-      'number_field': PlutoCell(value: 2021),
-      'select_field': PlutoCell(value: 'item2'),
-    },
-  ),
-  PlutoRow(
-    cells: {
-      'text_field': PlutoCell(value: 'Text cell value3'),
-      'number_field': PlutoCell(value: 2022),
-      'select_field': PlutoCell(value: 'item3'),
-    },
-  ),
-];
+//   /// Datetime Column definition
+// ];
+
+// List<PlutoRow> rows = [
+//   PlutoRow(cells: {
+//     'text_field': PlutoCell(value: 'Text cell value1'),
+//     'number_field': PlutoCell(value: 2020),
+//     'select_field': PlutoCell(value: 'item1'),
+//   }),
+//   PlutoRow(
+//     cells: {
+//       'text_field': PlutoCell(value: 'Text cell value2'),
+//       'number_field': PlutoCell(value: 2021),
+//       'select_field': PlutoCell(value: 'item2'),
+//     },
+//   ),
+//   PlutoRow(
+//     cells: {
+//       'text_field': PlutoCell(value: 'Text cell value3'),
+//       'number_field': PlutoCell(value: 2022),
+//       'select_field': PlutoCell(value: 'item3'),
+//     },
+//   ),
+// ];
